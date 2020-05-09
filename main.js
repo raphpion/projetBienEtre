cbdc626f96a6ec55f2ed853ff4f32029c5c96c58
Raphaël Pion <raph.pion@gmail.com>
2020-05-09 17:34:08 -0400

    Completed To-do List

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); /* activation des tooltips */
-    document.getElementById('youngRed').style.width = '14%'; /* grossissement de stat1 */
-
+    document.getElementById('youngRed').style.width = '16%'; /* grossissement de stat1 */
+    let x = document.getElementsByClassName('studentRed'); 
+    let i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.width = "24%";
+    }/* grossissement de stat 3 */
   });
--- myCharts.js
+++ myCharts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 let stat2 = document.getElementById('stat2').getContext('2d');
-let stat3 = document.getElementById('stat3').getContext('2d');
-let stat5 = document.getElementById('stat5').getContext('2d');
+// let stat3 = document.getElementById('stat3').getContext('2d');
+// let stat5 = document.getElementById('stat5').getContext('2d');
 
 let agedDistress = new Chart(stat2, {
     type: 'bar',
@@ -11,16 +11,16 @@ let agedDistress = new Chart(stat2, {
             label:'Proportion',
             data:[36, 31, 26, 22],
             backgroundColor:[
-                'rgba(197, 90, 17, 0.6)',
-                'rgba(244, 177, 131, 0.6)',
-                'rgba(255, 217, 102, 0.6)',
-                'rgba(80, 165, 52, 0.6)'],
+                'rgba(197, 90, 17, 0.8)',
+                'rgba(244, 177, 131, 0.3)',
+                'rgba(255, 217, 102, 0.3)',
+                'rgba(80, 165, 52, 0.3)'],
             borderWidth:1,
             borderColor:[
                 '#c55a11',
-                '#f4b183',
-                '#ffd966',
-                '#50a534']
+                'rgba(244, 177, 131, 0.5)',
+                'rgba(255, 217, 102, 0.5)',
+                'rgba(80, 165, 52, 0.5)'],
         }]
     },
     options:{
@@ -40,64 +40,66 @@ let agedDistress = new Chart(stat2, {
     }
 });
 
-let collegeAnxiety = new Chart(stat3, {
-    type: 'doughnut',
-    data:{
-        labels:["Haut niveau d'anxiété", 'Autres'],
-        datasets:[{
-            label:'Proportion',
-            data:[35.1, 64.9],
-            backgroundColor:[
-                'rgba(197, 90, 17, 0.6)',
-                'rgba(80, 165, 52, 0.6)'
-            ],
-            borderWidth:1,
-            borderColor:['#c55a11', '#50a534',]
-        }]
-    },
-    options:{
-        legend:{display:true},
-        tooltips:{enabled:false}
-    }
-});
+// let collegeAnxiety = new Chart(stat3, {
+//     type: 'doughnut',
+//     data:{
+//         labels:["Haut niveau d'anxiété", 'Autres'],
+//         datasets:[{
+//             label:'Proportion',
+//             data:[35.1, 64.9],
+//             backgroundColor:[
+//                 'rgba(197, 90, 17, 0.6)',
+//                 'rgba(80, 165, 52, 0.6)'
+//             ],
+//             borderWidth:1,
+//             borderColor:['#c55a11', '#50a534',]
+//         }]
+//     },
+//     options:{
+//         legend:{display:true},
+//         tooltips:{enabled:false}
+//     }
+// });
 
-let distressIncrease = new Chart(stat5, {
-    type: 'bar',
-    data:{
-        labels:['', '2011', '2017', ''],
-        datasets:[{
-            label:'Proportion',
-            data:[0, 21, 29, 0],
-            backgroundColor:[
-                'rgba(0, 0, 0, 0)',
-                'rgba(255, 217, 102, 0.6)',
-                'rgba(197, 90, 17, 0.6)',
-                'rgba(0, 0, 0, 0)'
-            ],
-            borderWidth:1,
-            borderColor:['rgba(0, 0, 0, 0)', '#ffd966', '#c55a11', 'rgba(0, 0, 0, 0)']
-        }, {
-        label:'Tendance',
-        type: 'line',
-        data:[0, 21, 29, 50]
-        }]
-    },
-    options:{
-        legend:{display:false},
-        tooltips:{enabled:false},
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    max:40
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Proportion (%)'
-                  }
-            }]
-        }
-    }
-})
+// let distressIncrease = new Chart(stat5, {
+//     type: 'bar',
+//     data:{
+//         labels:['', '2011', '2017', ''],
+//         datasets:[{
+//             label:'Proportion',
+//             data:[0, 21, 29, 0],
+//             backgroundColor:[
+//                 'rgba(0, 0, 0, 0)',
+//                 'rgba(255, 217, 102, 0.6)',
+//                 'rgba(197, 90, 17, 0.6)',
+//                 'rgba(0, 0, 0, 0)'
+//             ],
+//             borderWidth:1,
+//             borderColor:['rgba(0, 0, 0, 0)', '#ffd966', '#c55a11', 'rgba(0, 0, 0, 0)']
+//         }, {
+//         label:'Tendance',
+//         type: 'line',
+//         data:[undefined, 21, 29, undefined],
+//         steppedLine: true,
+//         backgroundColor:['rgba(0,0,0, 0)'],
+//         }]
+//     },
+//     options:{
+//         legend:{display:false},
+//         tooltips:{enabled:false},
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true,
+//                     max:40
+//                 },
+//                 scaleLabel: {
+//                     display: true,
+//                     labelString: 'Proportion (%)'
+//                   }
+//             }]
+//         }
+//     }
+// })
 
 });
